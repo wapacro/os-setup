fb412272706b1dc37f10379930e2a99ba7b39cee
Roman Ackermann <roman-ackermann@bluewin.ch>
2020-12-18 01:30:26 +0100

    Added Explorer alias

--- wsl/ubuntu/01-aliases.sh
+++ wsl/ubuntu/01-aliases.sh
@@ -3,6 +3,7 @@ alias cls="clear"
 alias gs="git status"
 alias par="php artisan"
 alias nace="node ace"
+alias explorer='explorer.exe `wslpath -w "$PWD"`'
 
 # Cloud Foundry
 alias cf-ssh="cf ssh -t -c \"/tmp/lifecycle/launcher /home/vcap/app bash ''\""
